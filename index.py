b66df5e0d81e52e22097f0a9522a15626612cae8
sathish-simple <sathish@cimplify.ai>
2025-02-21 11:23:22 +0530

    changes

--- index.py
+++ index.py
@@ -30,7 +30,12 @@ import os
 container_pdf, container_chat = st.columns([50, 50])
 # Initialize AWS Textract Client
 # Title of the app
-textract = boto3.client("textract", region_name="us-east-1")  # Change region if needed
+session = boto3.Session(
+    aws_access_key_id=st.secrets["access_key"],
+    aws_secret_access_key=st.secrets["secret_key"],
+    region_name='us-east-1'
+)
+textract = session.client("textract", region_name="us-east-1")  # Change region if needed
 
 def analyze_invoice(file_bytes):
     response = textract.analyze_expense(Document={"Bytes": file_bytes})
